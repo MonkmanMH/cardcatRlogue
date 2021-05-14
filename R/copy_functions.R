b64852b86e96c70c11bffdaab8317859f5f7894f
Martin Monkman <monkmanmh@yahoo.ca>
2021-05-13 21:00:37 -0700

    context

--- R/copy_functions.R
+++ R/copy_functions.R
@@ -1,12 +1,35 @@
 # functions
 
 
-# copy the book.bib file to the current directory
+# copy the book.bib & package.bib file to the current directory
 
 #' @export
 copy_bib <- function(){
   localpath <- getwd()
+  #
   filepath <- system.file("data", "book.bib", package = "cardcatRlogue")
+  file.copy(filepath, localpath, overwrite = TRUE)
+  #
+  filepath <- system.file("data", "packages.bib", package = "cardcatRlogue")
+  file.copy(filepath, localpath, overwrite = TRUE)
+}
+
+
+# copy the book.bib file to the current directory
+
+#' @export
+copy_bib_book <- function(){
+  localpath <- getwd()
+  #
+  filepath <- system.file("data", "book.bib", package = "cardcatRlogue")
+  file.copy(filepath, localpath, overwrite = TRUE)
+}
+
+#' @export
+copy_bib_package <- function(){
+  localpath <- getwd()
+  #
+  filepath <- system.file("data", "packages.bib", package = "cardcatRlogue")
   file.copy(filepath, localpath, overwrite = TRUE)
 }
 
--- README.md
+++ README.md
@@ -6,3 +6,12 @@ This package grew out of a frustration with creating and maintaining stand-alone
 
 In response, {cardcatRlogue} contains "book.bib" and "package.bib" files that will be used to populate the reference sections of all the books. The files can be copied to the appropriate bookdown-based book projects using the functions within the package.
 
+## {bookdown} and BibTeX resources
+
+Yihui Xie, [_bookdown: Authoring Books and Technical Documents with R Markdown_, 2.8 Citations](https://bookdown.org/yihui/bookdown/citations.html)
+
+Yihui Xie, Christophe Dervieux, Emily Riederer, [_R Markdown Cookbook_, 4.5 Bibliographies and citations](https://bookdown.org/yihui/rmarkdown-cookbook/bibliography.html)
+
+[BibTeX.org](http://www.bibtex.org/)
+
+[The quick BibTeX guide](https://www.bibtex.com/)—[The 14 BibTeX entry types](https://www.bibtex.com/e/entry-types/)
